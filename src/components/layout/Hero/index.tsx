42ff6866203a5ac4e7d94feed563d0e87a03a47e
Eduardo Guiraldelli Schelive <contact@eduardoschelive.com>
2025-11-10 10:11:36 -0300

    refactor: move HeroClient to components/HeroClient/index.tsx
    Follow the same pattern as other layout components like ArticleCard

--- src/components/layout/Hero/index.tsx
+++ src/components/layout/Hero/index.tsx
@@ -1,12 +1,11 @@
 import { getArticles } from '@/content/articles'
 import type { Locale } from 'next-intl'
 import { getLocale } from 'next-intl/server'
-import { HeroClient } from './HeroClient'
+import { HeroClient } from './components/HeroClient'
 
 async function Hero() {
   const locale = (await getLocale()) as Locale
 
-  // Fetch featured article
   const articles = await getArticles(locale, { limit: 1 })
   const featuredArticle = articles[0]
 
